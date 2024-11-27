8df50348b52d90b7e0d70f6e38209746ba4d3099
erne.undze@gmail.com <erne.undze@gmail.com>
2024-11-27 11:00:11 +0200

    fixed

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -54,11 +54,7 @@ function Portfolio()  {
                   <div className='border w-5/6 sm:w-3/4 rounded-xl py-8 flex text-white text-center items-center justify-start border-gray-600'>
                   <div class="min-w-fit w-1/12  ">01</div>
                   <div class="min-w-fit w-1/12 ">01</div>
-<<<<<<< HEAD
-                  <div class="min-w-fit w-1/3  text-start">Social media</div>
-=======
-                  <div class="min-w-fit w-1/3  text-start">socialmeedjhgjia</div>
->>>>>>> bcddcfb (Initial commit)
+                  <div class="min-w-fit w-1/3  text-start">Social Media</div>
               </div>
 
               <div className='border w-5/6 sm:w-3/4 rounded-xl py-8 flex text-white text-center items-center justify-start border-gray-600'>
